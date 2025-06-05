ea37264792e2d55f6ac2a54ec58be93ec410d92b
santipaez <sr.paez@alumno.um.edu.ar>
2025-06-05 18:47:21 -0300

    refactor(config): improve app configuration
    - remove unused UPLOAD_FOLDER and ALLOWED_EXTENSIONS
    - load SECRET_KEY and DATABASE_URL from environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,21 @@ from flask import Flask
 from flask_cors import CORS
 import os
 
-from app.config import SECRET_KEY, UPLOAD_FOLDER, DATABASE_URL
+from app.config import SECRET_KEY, DATABASE_URL
 from app.models.db import db, init_db
 
 def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = SECRET_KEY
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JSON_AS_ASCII'] = False  # Permitir caracteres UTF-8 en JSON
     
     CORS(app)
     db.init_app(app)
-    
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    
-    from app.routes.productos import productos_bp
+        
+    from app.routes.producto_routes import productos_bp
     
     app.register_blueprint(productos_bp, url_prefix='/v1')
     
--- app/config.py
+++ app/config.py
@@ -5,9 +5,5 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 
 load_dotenv(dotenv_path, override=True)
 
-
-
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/purple_db')
+DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24).hex())
-UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'app/static/images')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'} 
